d16dace290eba81362d0332ed1ef1ab9ef2d7e95
jorge <jorgeturrubia@hotmail.com>
2025-11-15 15:35:20 +0100

    d

---
+++ front/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'YOUR_SUPABASE_URL',
+  supabaseKey: 'YOUR_SUPABASE_ANON_KEY',
+  apiUrl: 'YOUR_PRODUCTION_API_URL'
+};
---
+++ front/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'YOUR_SUPABASE_URL',
+  supabaseKey: 'YOUR_SUPABASE_ANON_KEY',
+  apiUrl: 'http://localhost:5000/api'
+};
