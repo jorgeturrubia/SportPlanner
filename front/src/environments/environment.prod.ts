45e04d28b4fc9d43c979499412767cfbfb405d0b
jorge <jorgeturrubia@hotmail.com>
2025-11-15 22:45:23 +0100

    e

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://uyvcgsjrqgzpsbptmqxp.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5dmNnc2pycWd6cHNicHRtcXhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNTY3MTcsImV4cCI6MjA2NDgzMjcxN30.kTqCBlrvOrfeWu9e57DDLiToSPr_ZABUsKrlo5p-Nv8',
-  apiUrl: 'https://localhost:7146/api'
+  apiUrl: 'https://localhost:7152/api'
 };
